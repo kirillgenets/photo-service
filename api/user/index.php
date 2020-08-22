7a6a7b23095046bf058d5a2e8e4ac6130dad152c
Kirill Genets <kirill.genets2002@mail.ru>
2020-08-22 15:52:21 +0300

    Implement correct get users request

--- api/user/index.php
+++ api/user/index.php
@@ -26,30 +26,24 @@ $user = new User($db);
 
 $stmt = $user->read();
 
-if ($stmt->rowCount() > 0) {
-    $users_list = array();
-    $users_list["content"] = array();
+$users_list = array();
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
 
-        if (!is_matches_search($first_name) && !is_matches_search($surname) && !is_matches_search($phone)) {
-            continue;
-        }
-
-        $user_item = array(
-            "id" => $id,
-            "first_name" => $first_name,
-            "surname" => $surname,
-            "phone" => $phone
-        );
-
-        array_push($users_list["content"], $user_item);
+    if (!is_matches_search($first_name) && !is_matches_search($surname) && !is_matches_search($phone)) {
+        continue;
     }
 
-    http_response_code(200);
-    echo json_encode($users_list);
-} else {
-    http_response_code(404);
-    echo json_encode(array("message" => "Товары не найдены."), JSON_UNESCAPED_UNICODE);
+    $user_item = array(
+        "id" => $id,
+        "first_name" => $first_name,
+        "surname" => $surname,
+        "phone" => $phone
+    );
+
+    array_push($users_list, $user_item);
 }
+
+http_response_code(200);
+echo json_encode($users_list);
